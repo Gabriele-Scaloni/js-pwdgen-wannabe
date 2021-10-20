23458c19f6515eae1e0449d489bb1d3965b5e191
Gabriele-Scaloni <88436304+Gabriele-Scaloni@users.noreply.github.com>
2021-10-20 15:20:54 +0200

    finito

---
+++ javascript/esercizio.js
@@ -0,0 +1,12 @@
+
+
+let name = prompt("inserisci il tuo nome");
+
+let surname = prompt("inserisci il tuo cognome");
+
+let color = prompt("inserisci il tuo colore preferito");
+
+const number = "21";
+
+document.getElementById("password").innerHTML = `La tua nuova password è ${name}${surname}${color}${number}`;
+console.log(password);
